4e77ad9e36a397762383ded3e8ef174d1cf1e216
shbnm93 <shabnamhamidi1993@gmail.com>
2023-12-06 23:54:13 +0300

    migration

---
+++ review/employees/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+class Employee(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    photo = models.ImageField(upload_to='images')
+    designation = models.CharField(max_length=100)
+    email_address = models.EmailField(max_length=100, unique=True)
+    phone_number = models.CharField(max_length=12, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    uploaded_at = models.DateTimeField(auto_now=True)
