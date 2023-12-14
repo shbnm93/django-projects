0a16061d5c89b26a01bdd8016bcc8932f685e22e
shbnm93 <shabnamhamidi1993@gmail.com>
2023-12-15 00:17:13 +0300

    csrf token

--- django project 2/todo/admin.py
+++ django project 2/todo/admin.py
@@ -1,4 +1,10 @@
 from django.contrib import admin
 from .models import Task
 
-admin.site.register(Task)
+
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ('task', 'is_completed', 'updated_at')
+    search_fields = ('task',)
+
+
+admin.site.register(Task, TaskAdmin)
