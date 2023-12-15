d00b04ec548328cb4c4271db377125e362613af0
shbnm93 <shabnamhamidi1993@gmail.com>
2023-12-15 11:34:34 +0300

    add task functionality

--- django project 2/todo/views.py
+++ django project 2/todo/views.py
@@ -1,6 +1,9 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from .models import Task
 
 # Create your views here.
 def addTask(request):
-    return HttpResponse('The form is submitted')
+    task = request.POST['task']
+    Task.objects.create(task=task)
+    return redirect('home')
