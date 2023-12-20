492f3d60c8b18494c94d57cb4426ee6117c49bd8
shbnm93 <shabnamhamidi1993@gmail.com>
2023-12-21 01:18:24 +0300

    custom user model, category

---
+++ django project 3/accounts/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
+
+# Create your models here.
+class Account(AbstractBaseUser):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    username = models.CharField(max_length=50, unique=True)
+    email = models.EmailField(max_length=100, unique=True)
+    phone_number = models.CharField(max_length=50)
+
+    #required
+    date_joined = models.DateTimeField(auto_now_add = True)
+    last_login = models.DateTimeField(auto_now_add = True)
+    is_admin = models.
