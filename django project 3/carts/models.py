0ff7624771f71b46c2e6961e442fd3a42929c250
shbnm93 <shabnamhamidi1993@gmail.com>
2023-12-27 12:31:28 +0300

    add to cart using session keys, increment, decrement, remove

--- django project 3/carts/models.py
+++ django project 3/carts/models.py
@@ -14,8 +14,11 @@ class Cart(models.Model):
 class CartItem(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
-    Quantity = models.IntegerField()
+    quantity = models.IntegerField()
     is_active = models.BooleanField(default=True)
+
+    def sub_total(self):
+        return self.product.price * self.quantity
 
     def __str__(self):
         return self.product
