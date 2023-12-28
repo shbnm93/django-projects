a832dfaa47ba826202d59f03f1d23a616f3a0243
shbnm93 <shabnamhamidi1993@gmail.com>
2023-12-28 21:58:07 +0300

    paginator and search segments

--- django project 3/store/views.py
+++ django project 3/store/views.py
@@ -5,6 +5,7 @@ from carts.views import _cart_id
 from category.models import Category
 from .models import Product
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
+from django.db.models import Q
 
 # Create your views here.
 def store(request, category_slug=None):
@@ -19,7 +20,7 @@ def store(request, category_slug=None):
         paged_products = paginator.get_page(page)
         product_count = products.count()
     else:
-        products = Product.objects.all().filter(is_available=True)
+        products = Product.objects.all().filter(is_available=True).order_by('id')
         paginator = Paginator(products, 3)
         page = request.GET.get('page')
         paged_products = paginator.get_page(page)
@@ -44,3 +45,15 @@ def product_detail(request, category_slug, product_slug):
         'in_cart' : in_cart,
     }
     return render(request, 'store/product_detail.html', context)
+
+def search(request):
+    if 'keyword' in request.GET:
+        keyword = request.GET['keyword']
+        if keyword:
+            products = Product.objects.order_by('-created_date').filter(Q(description__icontains=keyword) | Q(product_name__icontains = keyword))
+            product_count = products.count()
+    context = {
+        'products' : products,
+        'product_count' : product_count,
+    }
+    return render(request, 'store/store.html', context)
