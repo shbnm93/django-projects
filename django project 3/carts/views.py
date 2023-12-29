a3d2798e7a4199f57157956f04fdfc254923723a
shbnm93 <shabnamhamidi1993@gmail.com>
2023-12-29 23:53:41 +0300

    product variations & variation manager

--- django project 3/carts/views.py
+++ django project 3/carts/views.py
@@ -1,7 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import get_object_or_404, redirect, render
 from .models import Cart, CartItem
-from store.models import Product
+from store.models import Product, Variation
 from django.core.exceptions import ObjectDoesNotExist
 # Create your views here.
 
@@ -14,12 +14,21 @@ def _cart_id(request):
 
 
 def add_cart(request, product_id):
-    color = request.GET['color']
-    size = request.GET['size']
-    return HttpResponse(color + ' ' + size)
-    exit()
-
     product = Product.objects.get(id=product_id)  #get the product
+    product_variation = []
+    if request.method == 'POST':
+        for item in request.POST:
+            key = item
+            value = request.POST[key]
+
+
+            try:
+                variation = Variation.objects.get(product=product,variation_category__iexact=key, variation_value__iexact=value)
+                product_variation.append(variation)
+            except:
+                pass
+
+    
     try:
         cart = Cart.objects.get(cart_id=_cart_id(request)) #get the cart using the cart_id present in the session
     except Cart.DoesNotExist:
