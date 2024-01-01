a8e7f4f57e3640e82a7483c8f140a93c195eddbc
shbnm93 <shabnamhamidi1993@gmail.com>
2024-01-02 01:37:24 +0300

    user login functionality

--- django project 3/greatcart/settings.py
+++ django project 3/greatcart/settings.py
@@ -135,3 +135,10 @@ STATICFILES_DIRS = [
 # media files configuration
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR /'media'
+
+
+from django.contrib.messages import constants as messages
+
+MESSAGE_TAGS = {
+    messages.ERROR: "danger",
+}
