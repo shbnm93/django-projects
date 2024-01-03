f4256d6f948f77e2484f91daa9c29c133320c4b3
shbnm93 <shabnamhamidi1993@gmail.com>
2024-01-03 23:58:17 +0300

    in progress

--- django project 3/greatcart/settings.py
+++ django project 3/greatcart/settings.py
@@ -147,6 +147,6 @@ MESSAGE_TAGS = {
 #SMTP configuration
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'shabnamhamidi1993@gmail.com'
-EMAIL_HOST_PASSWORD = ''
+EMAIL_HOST_USER = 'shabnamdev1993@gmail.com'
+EMAIL_HOST_PASSWORD = '123456Sa@'
 EMAIL_USE_TLS = True
