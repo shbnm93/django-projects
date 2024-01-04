30115ef2410e2551b61082fa7687cea65ea863e5
shbnm93 <shabnamhamidi1993@gmail.com>
2024-01-05 00:05:54 +0300

    checkout page design

--- django project 3/carts/views.py
+++ django project 3/carts/views.py
@@ -100,7 +100,7 @@ def remove_cart_item(request, product_id, cart_item_id):
     cart_item.delete()
     return redirect('cart')
 
-def cart(request, total=0, quantity=0, cart_item=None):
+def cart(request, total=0, quantity=0, cart_items=None):
     try:
         tax = 0
         grand_total = 0
@@ -122,3 +122,27 @@ def cart(request, total=0, quantity=0, cart_item=None):
         'grand_total' : grand_total,
     }
     return render(request, 'store/cart.html', context)
+
+
+def checkout(request, total=0, quantity=0, cart_items=None):
+    try:
+        tax = 0
+        grand_total = 0
+        cart = Cart.objects.get(cart_id=_cart_id(request))
+        cart_items = CartItem.objects.filter(cart=cart, is_active=True)
+        for cart_item in cart_items:
+            total += (cart_item.product.price * cart_item.quantity)
+            quantity += cart_item.quantity
+        tax = (2 * total)/100
+        grand_total = total + tax
+    except ObjectDoesNotExist:
+        pass
+
+    context = {
+        'total' : total,
+        'quantity' : quantity,
+        'cart_items' : cart_items,
+        'tax' : tax,
+        'grand_total' : grand_total,
+    }
+    return render(request, 'store/checkout.html', context)
